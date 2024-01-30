d140934e015d8545611aec89246104586d682a16
Alyson Alves <73599284+Alysonalves1@users.noreply.github.com>
2024-01-30 20:43:07 -0300

    primeiro codigo

---
+++ Conhecendo linguagem Ruby/Primeiro projeto em Ruby/saidaDeDados.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome: "
+name = gets.chomp
+
+print "Digite seu sobrenome: "
+surname = gets.chomp
+
+print "Digite sua idade: "
+age = gets.chomp.to_i
+
+print "Usuario: #{name.capitalize} #{surname.capitalize}, idade: #{age} anos. "
